54e0ab7ffce7e4dc8ed89123a38e51f3ee485a7d
Giga <giorgi.khundadze15b@geolab.edu.ge>
2024-01-30 20:48:10 +0400

    update styles & create header and footer components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+
+
+
+
+
+export const Header = () => {
+    return (
+        <header className="container">
+            <nav>
+                <div className="logo">
+                    <a href="#">
+                        <img src="https://geolab.hola.ge/wp-content/uploads/2023/10/geolab-ლოგო.png" alt="geolab logo" />
+                    </a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">page 3</a>
+                    </li>
+                    <li>
+                        <a href="#">page 4</a>
+                    </li>
+                    <li>
+                        <a href="#">page 5</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
