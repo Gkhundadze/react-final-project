a180e5d72d02683ef7e3da6dd7737b3dc6777fa7
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2024-02-07 08:08:47 +0400

    add search keyword suggestion functionality on food page (test)

--- src/components/pages/FoodPage.tsx
+++ src/components/pages/FoodPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
-import { RandomRecipeURL, foodApiKey } from "../../config/api/food"
+import { RandomRecipeURL, foodApiKey, suggestionsUrl } from "../../config/api/food"
 
 
 export const FoodPage = () => {
@@ -8,6 +8,8 @@ const [searchKeyword, setSearchKeyword] = useState('')
 const [req, setReq] = useState('')
 const [recipes, setRecipes] = useState([])
 const [baseURL, setBaseURL] = useState('')
+const [suggestions, setSuggestions] = useState([])
+const [recipeMessage, setRecipeMessage] = useState('')
 
 const handleSearch = (event:any) => {
     const searchKeyword = event.target.value
@@ -16,28 +18,48 @@ const handleSearch = (event:any) => {
     handleSearchQuery(searchKeyword)
     
 }
+const handleSuggestionClick = (event:any) => {
+    console.log(event.target.textContent);
+    setSearchKeyword(event.target.textContent)
+    handleSearchQuery(event.target.textContent)
+    setSuggestions([])
+}
 
 const handleSearchQuery = (query:string) => {
-    if(query.length > 3) {
+    if(query.length > 1) {
         const queryString = '?query=' + query
         setReq((RandomRecipeURL  + queryString + '&apiKey=' + foodApiKey))
+    }
+    else if(query.length < 1) {
+        setSuggestions([])
     }else {
         console.log('short query');
         
     }
 }
 const requestRecipe = () => {
+    setSuggestions([])
     axios.get(req)
     .then((res) => {
         console.log(res.data);
+        if(res.data.results.length === 0) {
+            setRecipeMessage('no recipe for that keyword')
+        }
         setBaseURL(res.data.baseUri)
         setRecipes(res.data.results)
     })
 }
+const requestSuggestions = () => {
+    axios.get(suggestionsUrl + '?number=10&query=' + searchKeyword + '&apiKey=' + foodApiKey )
+    .then((res) => {
+        console.log(res.data);
+        setSuggestions(res.data)
+    })
+}
 
+    
     useEffect(() => {
-        console.log(req);
-        
+        requestSuggestions()
     }, [req])
     return (
         <main>
@@ -45,10 +67,16 @@ const requestRecipe = () => {
             <div className="input-wrapper">
                 <input onInput={handleSearch} type="text" value={searchKeyword} />
                 <button onClick={requestRecipe}>search</button>
+                <div className="suggestions">
+                {suggestions?.map((suggestion:any) => {
+                    return (
+                        <div onClick={handleSuggestionClick} key={suggestion.id}>{suggestion.title}</div>
+                    )
+                })}
             </div>
-
+            </div>
             <div className="recipe-container">
-                {recipes.map((recipe:any) => {
+                {(recipes.length > 0) ? recipes.map((recipe:any) => {
                     return (
                         <div className="recipe-card" key={recipe.title}>
                             <img src={baseURL + recipe.image} alt={recipe.title} />
@@ -61,7 +89,8 @@ const requestRecipe = () => {
                             </div>
                         </div>
                     )
-                })}
+                }): <div className="no-recipe-message">{recipeMessage}</div>
+                }
             </div>
         </main>
     )
