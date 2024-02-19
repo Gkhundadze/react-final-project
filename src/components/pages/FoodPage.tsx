6c45dac506dd9886fc3dc87e05bede9e180fa5db
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2024-02-19 18:22:33 +0400

    working on visual side on news and search logic on fods page
    not done yet

--- src/components/pages/FoodPage.tsx
+++ src/components/pages/FoodPage.tsx
@@ -1,82 +1,121 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import axios from 'axios'
 import { RandomRecipeURL, foodApiKey, suggestionsUrl } from "../../config/api/food"
 
 
 export const FoodPage = () => {
-const [searchKeyword, setSearchKeyword] = useState('')
-const [req, setReq] = useState('')
-const [recipes, setRecipes] = useState([])
-const [baseURL, setBaseURL] = useState('')
-const [suggestions, setSuggestions] = useState([])
-const [recipeMessage, setRecipeMessage] = useState('')
+    const [searchKeyword, setSearchKeyword] = useState('')
+    const [req, setReq] = useState('')
+    const [recipes, setRecipes] = useState([])
+    const [baseURL, setBaseURL] = useState('')
+    const [suggestions, setSuggestions] = useState([])
+    const [recipeMessage, setRecipeMessage] = useState('')
+    const [isTyping, setIsTyping] = useState(false)
+    const [isFirstLoad, setIsFirstLoad ] = useState(true)
+    const wrapperRef = useRef(null);
 
-const handleSearch = (event:any) => {
-    const searchKeyword = event.target.value
-    console.log(event.target.value);
-    setSearchKeyword(searchKeyword)
-    handleSearchQuery(searchKeyword)
-    
-}
-const handleSuggestionClick = (event:any) => {
-    console.log(event.target.textContent);
-    setSearchKeyword(event.target.textContent)
-    handleSearchQuery(event.target.textContent)
-    setSuggestions([])
-}
-
-const handleSearchQuery = (query:string) => {
-    if(query.length > 1) {
-        const queryString = '?query=' + query
-        setReq((RandomRecipeURL  + queryString + '&apiKey=' + foodApiKey))
+    // outside click detection
+    const useOutsideAlerter = (ref: any) => {
+        useEffect(() => {
+            function handleClickOutside(event: any) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    // outside click
+                    setSuggestions([])
+                }
+            }
+            // Bind the event listener
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                // Unbind the event listener on clean up
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
     }
-    else if(query.length < 1) {
+    useOutsideAlerter(wrapperRef);
+    // outside click detection ^
+
+    const handleSearch = (event: any) => {
+        setIsTyping(true)
+        const searchKeyword = event.target.value
+        console.log(event.target.value);
+        setSearchKeyword(searchKeyword)
+        handleSearchQuery(searchKeyword)
+        setRecipeMessage('')
+        setIsFirstLoad(false)
+
+    }
+    const handleSuggestionClick = (event: any) => {
+        setIsTyping(false)
+        console.log(event.target.textContent);
+        setSearchKeyword(event.target.textContent)
+        handleSearchQuery(event.target.textContent)
         setSuggestions([])
-    }else {
-        console.log('short query');
-        
+        requestRecipe()
     }
-}
-const requestRecipe = () => {
-    setSuggestions([])
-    axios.get(req)
-    .then((res) => {
-        console.log(res.data);
-        if(res.data.results.length === 0) {
-            setRecipeMessage('no recipe for that keyword')
-        }
-        setBaseURL(res.data.baseUri)
-        setRecipes(res.data.results)
-    })
-}
-const requestSuggestions = () => {
-    axios.get(suggestionsUrl + '?number=5&query=' + searchKeyword + '&apiKey=' + foodApiKey )
-    .then((res) => {
-        console.log(res.data);
-        setSuggestions(res.data)
-    })
-}
 
-    
+    const handleSearchQuery = (query: string) => {
+        if (query.length > 1) {
+            const queryString = '?query=' + query
+            setReq((RandomRecipeURL + queryString + '&apiKey=' + foodApiKey))
+        }
+        else if (query.length < 1) {
+            setSuggestions([])
+        } else {
+            console.log('short query');
+
+        }
+    }
+    const requestRecipe = () => {
+        setIsTyping(false)
+        setSuggestions([])
+        axios.get(req)
+            .then((res) => {
+                console.log(res.data);
+                if (res.data.results.length === 0) {
+                    setRecipeMessage('no recipe for that keyword')
+                }
+                setBaseURL(res.data.baseUri)
+                setRecipes(res.data.results)
+            })
+    }
+    const requestSuggestions = () => {
+        axios.get(suggestionsUrl + '?number=5&query=' + searchKeyword + '&apiKey=' + foodApiKey)
+            .then((res) => {
+                console.log(res.data);
+                setSuggestions(res.data)
+                setRecipeMessage('no recipe for that keyword')
+            })
+    }
+
+
+
     useEffect(() => {
         requestSuggestions()
     }, [req])
     return (
         <main>
             <h1 className="recipe-page-title">search recipe by your needs</h1>
-            <div className="input-wrapper">
-                <input onInput={handleSearch} type="search" value={searchKeyword} />
-                <button onClick={requestRecipe}>search</button>
-                <div className="suggestions">
-                {suggestions?.map((suggestion:any) => {
-                    return (
-                        <div onClick={handleSuggestionClick} key={suggestion.id}>{suggestion.title}</div>
-                    )
-                })}
-            </div>
-            </div>
+            <form className="input-wrapper">
+                <input onChange={handleSearch} type="search" value={searchKeyword} />
+                <button type="submit" onClick={requestRecipe}>search</button>
+                {suggestions.length > 0 && isTyping ? //NOT DONE
+                    <ul ref={wrapperRef} className="suggestions">
+                        {suggestions.map((suggestion: any) => {
+                            return (
+                                <li className="suggestion-item"
+                                    onClick={handleSuggestionClick}
+                                    key={suggestion.id}
+                                >
+                                    {suggestion.title}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    : null
+                }
+            </form>
             <div className="recipe-container">
-                {(recipes.length > 0) ? recipes.map((recipe:any) => {
+                {(recipes.length > 0) ? recipes.map((recipe: any) => {
                     return (
                         <div className="recipe-card" key={recipe.title}>
                             <img src={baseURL + recipe.image} alt={recipe.title} />
@@ -89,7 +128,9 @@ const requestSuggestions = () => {
                             </div>
                         </div>
                     )
-                }): <div className="no-recipe-message">{recipeMessage}</div>
+                }) : !isFirstLoad  ? 
+                                    <div className="no-recipe-message">{recipeMessage}</div>
+                                    : null
                 }
             </div>
         </main>
--- src/components/pages/news/NewsArticle.tsx
+++ src/components/pages/news/NewsArticle.tsx
@@ -4,15 +4,18 @@ import brokenImage from '../../../assets/images/broken-image.gif'
 export const NewsArticle = (props:any) => {
     const { article } = props
     
-
+    const imgErrorHandler = (e:any) => {
+        e.target.src = brokenImage
+    }
     return (
         <div className="article-card">
             <img
                 src={article.image_url ? article.image_url : brokenImage}
                 alt={article.title}
+                onError={imgErrorHandler}
             />
-            <h3>{article.title}</h3>
-            <p>{article.description ? article.description : 'No Description Available'}</p>
+            <h3 title={article.title}>{article.title}</h3>
+            <p title={article.description}>{article.description ? article.description : 'No Description Available'}</p>
         </div>
     )
 }
